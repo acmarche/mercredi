4d85107a34d5d58da802a79a9834190f1c84af9e
jfsenechal <jfsenechal@gmail.com>
2021-04-27 13:28:57 +0200

    desactive copie

--- src/Mailer/SetToListener.php
+++ src/Mailer/SetToListener.php
@@ -16,7 +16,7 @@ class SetToListener implements EventSubscriberInterface
         if (!$email instanceof Email) {
             return;
         }
-        $email->bcc(new Address('jf@marche.be', 'Test mail'));
+        //$email->bcc(new Address('xx@xx.be', 'Test mail'));
     }
 
     public static function getSubscribedEvents()
