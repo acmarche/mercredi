12d5d1d4975c568d6da7d465e9031dc0aa311e15
jfsenechal <jfsenechal@gmail.com>
2021-07-13 14:21:21 +0200

    tmp active const

--- src/Entity/Accueil.php
+++ src/Entity/Accueil.php
@@ -22,9 +22,11 @@ use AcMarche\Mercredi\Entity\Facture\FactureAccueil;
 /**
  * Class Accueil.
  *
- * @ORM\Table("accueil")
+ * @ORM\Table("accueil", uniqueConstraints={
+ *     @ORM\UniqueConstraint(columns={"date_jour", "enfant_id", "heure"})
+ * })
  * @ORM\Entity(repositoryClass="AcMarche\Mercredi\Accueil\Repository\AccueilRepository")
- * UniqueEntity(fields={"date_jour", "enfant", "heure"}, message="L'enfant est déjà inscrit à cette date")
+ * @UniqueEntity(fields={"date_jour", "enfant", "heure"}, message="L'enfant est déjà inscrit à cette date")
  */
 class Accueil implements TimestampableInterface, UuidableInterface
 {
