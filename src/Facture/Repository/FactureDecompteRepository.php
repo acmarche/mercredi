b9387b6a98b68006b904f591f0cdad7a0c88fb4c
jfsenechal <jfsenechal@gmail.com>
2021-09-29 13:46:56 +0200

    add decompte, edit reduc, complement

---
+++ src/Facture/Repository/FactureDecompteRepository.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace AcMarche\Mercredi\Facture\Repository;
+
+use AcMarche\Mercredi\Doctrine\OrmCrudTrait;
+use AcMarche\Mercredi\Entity\Facture\Facture;
+use AcMarche\Mercredi\Entity\Facture\FactureDecompte;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method FactureDecompte|null find($id, $lockMode = null, $lockVersion = null)
+ * @method FactureDecompte|null findOneBy(array $criteria, array $orderBy = null)
+ * @method FactureDecompte[]    findAll()
+ * @method FactureDecompte[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class FactureDecompteRepository extends ServiceEntityRepository
+{
+    use OrmCrudTrait;
+
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, FactureDecompte::class);
+    }
+
+    /**
+     * @param Facture $facture
+     * @return array|FactureDecompte[]
+     */
+    public function findByFacture(Facture $facture): array
+    {
+        return $this->createQueryBuilder('facture_decompte')
+            ->andWhere('facture_decompte.facture = :fact')
+            ->setParameter('fact', $facture)
+            ->getQuery()->getResult();
+    }
+
+}
