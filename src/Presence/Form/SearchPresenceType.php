d6b7836fb512ee334b03e1f359934aace032093a
jfsenechal <jfsenechal@gmail.com>
2021-10-26 13:59:59 +0200

    ajout label jour présences dans le listing

--- src/Presence/Form/SearchPresenceType.php
+++ src/Presence/Form/SearchPresenceType.php
@@ -3,9 +3,11 @@
 namespace AcMarche\Mercredi\Presence\Form;
 
 use AcMarche\Mercredi\Ecole\Repository\EcoleRepository;
+use AcMarche\Mercredi\Ecole\Utils\EcoleUtils;
 use AcMarche\Mercredi\Entity\Scolaire\Ecole;
 use AcMarche\Mercredi\Entity\Jour;
 use AcMarche\Mercredi\Jour\Repository\JourRepository;
+use AcMarche\Mercredi\Utils\DateUtils;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
@@ -23,6 +25,14 @@ final class SearchPresenceType extends AbstractType
                     'class' => Jour::class,
                     'placeholder' => 'Choisissez une date',
                     'query_builder' => fn(JourRepository $jourRepository) => $jourRepository->getQlNotPlaine(),
+                    'choice_label' => function (Jour $jour) {
+                        $peda = '';
+                        if ($jour->isPedagogique()) {
+                            $ecoles = EcoleUtils::getNamesEcole($jour->getEcoles());
+                            $peda = '(Pédagogique '.$ecoles.')';
+                        }
+                        return ucfirst(DateUtils::formatFr($jour->getDatejour()).' '.$peda);
+                    },
                     'group_by' => fn($jour, $key, $id) => $jour->getDateJour()->format('Y'),
                 ]
             )
