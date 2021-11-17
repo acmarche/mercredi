fe712a43fcab78cf67d273a26df3608a54c72030
jfsenechal <jfsenechal@gmail.com>
2021-11-17 14:34:57 +0100

    checkup account

--- src/Controller/Admin/CheckupController.php
+++ src/Controller/Admin/CheckupController.php
@@ -3,7 +3,9 @@
 namespace AcMarche\Mercredi\Controller\Admin;
 
 use AcMarche\Mercredi\Enfant\Repository\EnfantRepository;
+use AcMarche\Mercredi\Security\Role\MercrediSecurityRole;
 use AcMarche\Mercredi\Tuteur\Repository\TuteurRepository;
+use AcMarche\Mercredi\User\Repository\UserRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,12 +21,17 @@ final class CheckupController extends AbstractController
 {
     private EnfantRepository $enfantRepository;
     private TuteurRepository $tuteurRepository;
+    private UserRepository $userRepository;
     private $tutru = null;
 
-    public function __construct(EnfantRepository $enfantRepository, TuteurRepository $tuteurRepository)
-    {
+    public function __construct(
+        EnfantRepository $enfantRepository,
+        TuteurRepository $tuteurRepository,
+        UserRepository $userRepository
+    ) {
         $this->enfantRepository = $enfantRepository;
         $this->tuteurRepository = $tuteurRepository;
+        $this->userRepository = $userRepository;
     }
 
     /**
@@ -77,6 +84,46 @@ final class CheckupController extends AbstractController
             '@AcMarcheMercrediAdmin/default/index.html.twig',
             [
 
+            ]
+        );
+    }
+
+    /**
+     * @Route("/accounts", name="mercredi_admin_accounts")
+     */
+    public function accounts(): Response
+    {
+        $bad = [];
+        $users = $this->userRepository->findAllOrderByNom();
+        foreach ($users as $user) {
+            if ($user->getRoles() < 1) {
+                $bad[] = ['error' => 'Aucun rôle', 'user' => $user];
+                continue;
+            }
+            if ($user->hasRole(MercrediSecurityRole::ROLE_PARENT)) {
+                if (count($user->getTuteurs()) === 0) {
+                    $bad[] = ['error' => 'Rôle parent, mais aucun parent associé', 'user' => $user];
+                    continue;
+                }
+            }
+            if ($user->hasRole(MercrediSecurityRole::ROLE_ANIMATEUR)) {
+                if (count($user->getAnimateurs()) === 0) {
+                    $bad[] = ['error' => 'Rôle animateur, mais aucun animateur associé', 'user' => $user];
+                    continue;
+                }
+            }
+            if ($user->hasRole(MercrediSecurityRole::ROLE_ECOLE)) {
+                if (count($user->getEcoles()) === 0) {
+                    $bad[] = ['error' => 'Rôle école, mais aucune école associée', 'user' => $user];
+                    continue;
+                }
+            }
+        }
+
+        return $this->render(
+            '@AcMarcheMercrediAdmin/checkup/accounts.html.twig',
+            [
+                'users' => $bad,
             ]
         );
     }
