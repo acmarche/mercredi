b74de1d759fadf9bb6855cd8697a83a1da50beb2
jfsenechal <jfsenechal@gmail.com>
2021-11-18 10:01:23 +0100

    search by email

--- src/Tuteur/Repository/TuteurRepository.php
+++ src/Tuteur/Repository/TuteurRepository.php
@@ -34,7 +34,7 @@ final class TuteurRepository extends ServiceEntityRepository
         return $this->createQueryBuilder('tuteur')
             ->leftJoin('tuteur.relations', 'relations', 'WITH')
             ->addSelect('relations')
-            ->andWhere('tuteur.nom LIKE :keyword OR tuteur.prenom LIKE :keyword')
+            ->andWhere('tuteur.nom LIKE :keyword OR tuteur.prenom LIKE :keyword OR tuteur.email_conjoint LIKE :keyword OR tuteur.email LIKE :keyword')
             ->setParameter('keyword', '%'.$keyword.'%')
             ->andwhere('tuteur.archived = :archive')
             ->setParameter('archive', $archived)
