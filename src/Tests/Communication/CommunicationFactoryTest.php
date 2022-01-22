4dd6416b3265b528be0e990554c069c2bc49779d
jfsenechal <jfsenechal@gmail.com>
2022-01-22 16:43:51 +0100

    fix test

--- src/Tests/Communication/CommunicationFactoryTest.php
+++ src/Tests/Communication/CommunicationFactoryTest.php
@@ -3,6 +3,7 @@
 namespace AcMarche\Mercredi\Tests\Communication;
 
 use AcMarche\Mercredi\Entity\Facture\Facture;
+use AcMarche\Mercredi\Entity\Scolaire\Ecole;
 use AcMarche\Mercredi\Entity\Tuteur;
 use AcMarche\Mercredi\Facture\Factory\CommunicationFactoryHotton;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
@@ -19,12 +20,17 @@ class CommunicationFactoryTest extends KernelTestCase
         $container = static::getContainer();
         $communicationFactory = $container->get(CommunicationFactoryHotton::class);
         $facture = new Facture(new Tuteur());
+        $id = ' ';//je ne sais pas faire $facture->setId()
         $facture->setMois('09-2021');
+        $ecole = new Ecole();
+        $ecole->setNom('Hotton');
+        $ecole->setAbreviation('Hot');
+        $facture->ecolesListing = [$ecole];
         $year = 2021;
         $short = substr($year, -2, 2);
         self::assertSame('21', $short);
-        $communication = $communicationFactory->generate($facture);
+        $communication = $communicationFactory->generateForPresence($facture);
 
-        self::assertSame('ez', $communication);
+        self::assertSame('Hot'.' '.$id.' '.$facture->getMois(), $communication);
     }
 }
