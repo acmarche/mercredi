a68d16a8922fdc60bc1bd9c0a5cf725939c47772
jfsenechal <jfsenechal@gmail.com>
2022-01-26 11:46:19 +0100

    add tests

---
+++ src/Facture/Render/FacturePdfPresenceMarche.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace AcMarche\Mercredi\Facture\Render;
+
+use AcMarche\Mercredi\Contrat\Facture\FacturePdfPresenceInterface;
+use AcMarche\Mercredi\Entity\Facture\FacturePresence;
+use AcMarche\Mercredi\Facture\FactureInterface;
+use AcMarche\Mercredi\Facture\Repository\FacturePresenceRepository;
+use AcMarche\Mercredi\Facture\Utils\FactureUtils;
+use AcMarche\Mercredi\Organisation\Repository\OrganisationRepository;
+use Twig\Environment;
+
+class FacturePdfPresenceMarche implements FacturePdfPresenceInterface
+{
+    public function __construct(
+        private Environment $environment,
+        private OrganisationRepository $organisationRepository,
+        private FactureUtils $factureUtils,
+        private FacturePresenceRepository $facturePresenceRepository
+    ) {
+    }
+
+    public function render(FactureInterface $facture): string
+    {
+        $content = $this->prepareContent($facture);
+
+        return $this->environment->render(
+            '@AcMarcheMercrediAdmin/facture/hotton/pdf.html.twig',
+            [
+                'content' => $content,
+            ]
+        );
+    }
+
+    public function renderMultiple(array $factures): string
+    {
+        $content = '';
+        foreach ($factures as $facture) {
+            $content .= $this->prepareContent($facture);
+            $content .= '<div class="page-breaker"></div>';
+        }
+
+        return $this->environment->render(
+            '@AcMarcheMercrediAdmin/facture/hotton/pdf.html.twig',
+            [
+                'content' => $content,
+            ]
+        );
+    }
+
+    private function prepareContent(FactureInterface $facture): string
+    {
+        $organisation = $this->organisationRepository->getOrganisation();
+        $data = [
+            'enfants' => [],
+            'cout' => 0,
+        ];
+        //init
+        foreach ($this->factureUtils->getEnfants($facture) as $slug => $enfant) {
+            $data['enfants'][$slug] = [
+                'enfant' => $enfant,
+                'cout' => 0,
+                'peda' => 0,
+                'mercredi' => 0,
+                'accueils' => [
+                    'Soir' => [
+                        'nb' => 0,
+                        'cout' => 0,
+                    ],
+                    'Matin' => [
+                        'nb' => 0,
+                        'cout' => 0,
+                        
+                    ],
+                ],
+            ];
+        }
+
+        $tuteur = $facture->getTuteur();
+        $facturePresences = $this->facturePresenceRepository->findByFactureAndType(
+            $facture,
+            FactureInterface::OBJECT_PRESENCE
+        );
+        $factureAccueils = $this->facturePresenceRepository->findByFactureAndType(
+            $facture,
+            FactureInterface::OBJECT_ACCUEIL
+        );
+
+        foreach ($facturePresences as $facturePresence) {
+            $data = $this->groupPresences($facturePresence, $data);
+        }
+
+        foreach ($factureAccueils as $factureAccueil) {
+            $data = $this->groupAccueils($factureAccueil, $data);
+        }
+
+        foreach ($data['enfants'] as $enfant) {
+            $data['cout'] += $enfant['cout'];
+        }
+
+        return $this->environment->render(
+            '@AcMarcheMercrediAdmin/facture/hotton/_presence_content_pdf.html.twig',
+            [
+                'facture' => $facture,
+                'tuteur' => $tuteur,
+                'organisation' => $organisation,
+                'data' => $data,
+                'countAccueils' => \count($factureAccueils),
+                'countPresences' => \count($facturePresences),
+            ]
+        );
+    }
+
+    private function groupAccueils(FacturePresence $facturePresence, array $data): array
+    {
+        $enfant = $facturePresence->getNom().' '.$facturePresence->getPrenom();
+        $slug = $this->factureUtils->slugger->slug($enfant);
+        $heure = $facturePresence->getHeure();
+        $duree = $facturePresence->getDuree();
+        $data['enfants'][$slug->toString()]['cout'] += $facturePresence->getCoutCalculated();
+        $data['enfants'][$slug->toString()]['accueils'][$heure]['nb'] += $duree;
+
+        return $data;
+    }
+
+    private function groupPresences(FacturePresence $facturePresence, array $data): array
+    {
+        $enfant = $facturePresence->getNom().' '.$facturePresence->getPrenom();
+        $slug = $this->factureUtils->slugger->slug($enfant);
+        if ($facturePresence->isPedagogique()) {
+            ++$data['enfants'][$slug->toString()]['peda'];
+        }
+        if (! $facturePresence->isPedagogique()) {
+            ++$data['enfants'][$slug->toString()]['mercredi'];
+        }
+        $data['enfants'][$slug->toString()]['cout'] += $facturePresence->getCoutCalculated();
+
+        return $data;
+    }
+}
--- src/Tests/Calculator/CalculatorMarcheTest.php
+++ src/Tests/Calculator/CalculatorMarcheTest.php
@@ -6,16 +6,18 @@ use AcMarche\Mercredi\Contrat\Presence\PresenceCalculatorInterface;
 use AcMarche\Mercredi\Data\MercrediConstantes;
 use AcMarche\Mercredi\Entity\Enfant;
 use AcMarche\Mercredi\Entity\Jour;
+use AcMarche\Mercredi\Entity\Plaine\Plaine;
 use AcMarche\Mercredi\Entity\Presence\Presence;
 use AcMarche\Mercredi\Entity\Tuteur;
 use DateTime;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-class CalculatorHottonTest extends KernelTestCase
+class CalculatorMarcheTest extends KernelTestCase
 {
     public function testAbsent(): void
     {
-        $calculator = $this->getCalculator();
+        $container = static::getContainer();
+        $calculator = $container->get(PresenceCalculatorInterface::class);
         $tuteur = new Tuteur();
         $enfant = new Enfant();
         $jour = new Jour();
@@ -27,7 +29,9 @@ class CalculatorHottonTest extends KernelTestCase
 
     public function testOrdre(): void
     {
-        $calculator = $this->getCalculator();
+        $container = static::getContainer();
+        $calculator = $container->get(PresenceCalculatorInterface::class);
+
         $jour = new  Jour(new DateTime());
         $jour->setPrix1(3);
         $jour->setPrix2(2);
@@ -53,17 +57,39 @@ class CalculatorHottonTest extends KernelTestCase
 
         $presence = new Presence(new Tuteur(), new Enfant(), $jour);
         $presence->setHalf(true);
-        self::assertSame(2.0, $calculator->getPrixByOrdre($presence, 0));
-        self::assertSame(2.0, $calculator->getPrixByOrdre($presence, 1));
+        self::assertSame(3.0, $calculator->getPrixByOrdre($presence, 0));
+        self::assertSame(3.0, $calculator->getPrixByOrdre($presence, 1));
         self::assertSame(2.0, $calculator->getPrixByOrdre($presence, 2));
-        self::assertSame(2.0, $calculator->getPrixByOrdre($presence, 3));
-        self::assertSame(2.0, $calculator->getPrixByOrdre($presence, 4));
+        self::assertSame(1.0, $calculator->getPrixByOrdre($presence, 3));
+        self::assertSame(1.0, $calculator->getPrixByOrdre($presence, 4));
         $presence->setHalf(false);
         self::assertSame(3.0, $calculator->getPrixByOrdre($presence, 0));
         self::assertSame(3.0, $calculator->getPrixByOrdre($presence, 1));
-        self::assertSame(3.0, $calculator->getPrixByOrdre($presence, 2));
-        self::assertSame(3.0, $calculator->getPrixByOrdre($presence, 3));
-        self::assertSame(3.0, $calculator->getPrixByOrdre($presence, 4));
+        self::assertSame(2.0, $calculator->getPrixByOrdre($presence, 2));
+        self::assertSame(1.0, $calculator->getPrixByOrdre($presence, 3));
+        self::assertSame(1.0, $calculator->getPrixByOrdre($presence, 4));
+    }
+
+    public function testPlaine()
+    {
+        $calculator = $this->getCalculator();
+        $plaine = new Plaine();
+        $plaine->setPrix1(3);
+        $plaine->setPrix2(2);
+        $plaine->setPrix3(1);
+
+        $jour = new  Jour(new DateTime());
+        $jour->setPrix1(3);
+        $jour->setPrix2(2);
+        $jour->setPrix3(1);
+        $jour->setPlaine($plaine);
+
+        $presence = new Presence(new Tuteur(), new Enfant(), $jour);
+        self::assertSame(3.0, $calculator->calculatePlaine($presence, 0));
+        self::assertSame(3.0, $calculator->calculatePlaine($presence, 1));
+        self::assertSame(2.0, $calculator->calculatePlaine($presence, 2));
+        self::assertSame(1.0, $calculator->calculatePlaine($presence, 3));
+        self::assertSame(1.0, $calculator->calculatePlaine($presence, 4));
     }
 
     private function getCalculator(): PresenceCalculatorInterface
