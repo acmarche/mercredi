829d6cd796ecad37702cc5838f3cb1075e67b99e
jfsenechal <jfsenechal@gmail.com>
2022-09-06 16:38:23 +0200

    TwigConfig

--- config/packages/twig.php
+++ config/packages/twig.php
@@ -1,27 +1,22 @@
 <?php
 
-use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Config\TwigConfig;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->extension(
-        'twig',
-        [
-            'form_themes' => ['bootstrap_5_layout.html.twig'],
-            'paths' => [
-                '%kernel.project_dir%/src/AcMarche/Mercredi/templates/admin' => 'AcMarcheMercrediAdmin',
-                '%kernel.project_dir%/src/AcMarche/Mercredi/templates/parent' => 'AcMarcheMercrediParent',
-                '%kernel.project_dir%/src/AcMarche/Mercredi/templates/ecole' => 'AcMarcheMercrediEcole',
-                '%kernel.project_dir%/src/AcMarche/Mercredi/templates/animateur' => 'AcMarcheMercrediAnimateur',
-                '%kernel.project_dir%/src/AcMarche/Mercredi/templates/email' => 'AcMarcheMercrediEmail',
-            ],
-            'globals' => [
-                'mercredi_register' => '%env(MERCREDI_REGISTER)%',
-                'mercredi_accueil' => '%env(MERCREDI_ACCUEIL)%',
-                'mercredi_paiement' => '%env(MERCREDI_PAIEMENT)%',
-                'presence_nb_days' => '%env(MERCREDI_PRESENCE_DEADLINE_DAYS)%',
-                'pedagogique_nb_days' => '%env(MERCREDI_PEDAGOGIQUE_DEADLINE_DAYS)%',
-                'bootcdn' => 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css',
-            ],
-        ]
-    );
+return static function (TwigConfig $twig) {
+    $twig
+        ->path('%kernel.project_dir%/src/AcMarche/Mercredi/templates/admin', 'AcMarcheMercrediAdmin',)
+        ->path('%kernel.project_dir%/src/AcMarche/Mercredi/templates/parent', 'AcMarcheMercrediParent',)
+        ->path('%kernel.project_dir%/src/AcMarche/Mercredi/templates/ecole', 'AcMarcheMercrediEcole',)
+        ->path('%kernel.project_dir%/src/AcMarche/Mercredi/templates/animateur', 'AcMarcheMercrediAnimateur',)
+        ->path('%kernel.project_dir%/src/AcMarche/Mercredi/templates/email', 'AcMarcheMercrediEmail',)
+        ->formThemes(['bootstrap_5_layout.html.twig'])
+        ->global('mercredi_register', filter_var('%env(MERCREDI_REGISTER)%', FILTER_VALIDATE_BOOLEAN))
+        ->global('mercredi_accueil', filter_var('%env(MERCREDI_ACCUEIL)%', FILTER_VALIDATE_BOOLEAN))
+        ->global('mercredi_paiement', filter_var('%env(MERCREDI_PAIEMENT)%', FILTER_VALIDATE_BOOLEAN))
+        ->global('presence_nb_days', '%env(MERCREDI_PRESENCE_DEADLINE_DAYS)%',)
+        ->global('pedagogique_nb_days', '%env(MERCREDI_PEDAGOGIQUE_DEADLINE_DAYS)%',)
+        ->global(
+            'bootcdn',
+            'https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css',
+        );
 };
