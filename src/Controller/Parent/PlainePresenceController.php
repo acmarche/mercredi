13b191c63fabe07cab8b1d4ab731cd2c0ba563e8
jfsenechal <jfsenechal@gmail.com>
2022-09-27 20:45:31 +0200

    clean

--- src/Controller/Parent/PlainePresenceController.php
+++ src/Controller/Parent/PlainePresenceController.php
@@ -14,7 +14,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use function Sodium\add;
 
 class PlainePresenceController extends AbstractController
 {
