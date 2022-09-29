884c4c17eedc9f0bef71f8c68609b6d3d2527bbf
jfsenechal <jfsenechal@gmail.com>
2022-09-29 13:01:47 +0200

    raz

--- src/Plaine/Handler/PlaineHandlerHotton.php
+++ src/Plaine/Handler/PlaineHandlerHotton.php
@@ -3,38 +3,21 @@
 namespace AcMarche\Mercredi\Plaine\Handler;
 
 use AcMarche\Mercredi\Contrat\Plaine\PlaineHandlerInterface;
-use AcMarche\Mercredi\Contrat\Presence\PresenceHandlerInterface;
 use AcMarche\Mercredi\Entity\Enfant;
 use AcMarche\Mercredi\Entity\Plaine\Plaine;
-use AcMarche\Mercredi\Entity\Presence\Presence;
 use AcMarche\Mercredi\Entity\Tuteur;
-use AcMarche\Mercredi\Facture\Handler\FacturePlaineHandler;
-use AcMarche\Mercredi\Mailer\Factory\AdminEmailFactory;
-use AcMarche\Mercredi\Mailer\Factory\FactureEmailFactory;
-use AcMarche\Mercredi\Mailer\NotificationMailer;
-use AcMarche\Mercredi\Plaine\Repository\PlainePresenceRepository;
-use AcMarche\Mercredi\Tuteur\Utils\TuteurUtils;
-use DateTime;
 use Doctrine\Common\Collections\Collection;
 use Exception;
-use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
 
 class PlaineHandlerHotton implements PlaineHandlerInterface
 {
-    public function __construct(
-        private PlainePresenceRepository $plainePresenceRepository,
-        private FacturePlaineHandler $facturePlaineHandler,
-        private FactureEmailFactory $factureEmailFactory,
-        private NotificationMailer $notificationMailer,
-        private AdminEmailFactory $adminEmailFactory,
-        private PresenceHandlerInterface $presenceHandler
-    ) {
+    public function __construct()
+    {
     }
 
-    public function handleAddEnfant(Plaine $plaine, Tuteur $tuteur, Enfant $enfant, iterable $jours = []): void
+    public function handleAddEnfant(Plaine $plaine, Tuteur $tuteur, Enfant $enfant, iterable $jours = []): array
     {
-       // $jours = $plaine->getJours();
-        $this->presenceHandler->handleNew($tuteur, $enfant, $jours);
+        return [];
     }
 
     public function handleEditPresences(
@@ -43,36 +26,17 @@ class PlaineHandlerHotton implements PlaineHandlerInterface
         array $currentJours,
         Collection $newJours
     ): void {
-        $enMoins = array_diff($currentJours, $newJours->toArray());
-        $enPlus = array_diff($newJours->toArray(), $currentJours);
 
-        foreach ($enPlus as $jour) {
-            $presence = new Presence($tuteur, $enfant, $jour);
-            $this->plainePresenceRepository->persist($presence);
-        }
-
-        foreach ($enMoins as $jour) {
-            $presence = $this->plainePresenceRepository->findOneByEnfantJour($enfant, $jour);
-            if (null !== $presence) {
-                $this->plainePresenceRepository->remove($presence);
-            }
-        }
-
-        $this->plainePresenceRepository->flush();
     }
 
     public function removeEnfant(Plaine $plaine, Enfant $enfant): void
     {
-        $presences = $this->plainePresenceRepository->findByPlaineAndEnfant($plaine, $enfant);
-        foreach ($presences as $presence) {
-            $this->plainePresenceRepository->remove($presence);
-        }
-        $this->plainePresenceRepository->flush();
+
     }
 
     public function isRegistrationFinalized(Plaine $plaine, Tuteur $tuteur): bool
     {
-        return [] !== $this->plainePresenceRepository->findByPlaineAndTuteur($plaine, $tuteur, true);
+        return false;
     }
 
     /**
@@ -80,42 +44,6 @@ class PlaineHandlerHotton implements PlaineHandlerInterface
      */
     public function confirm(Plaine $plaine, Tuteur $tuteur): void
     {
-        $inscriptions = $this->plainePresenceRepository->findByPlaineAndTuteur($plaine, $tuteur);
-        foreach ($inscriptions as $inscription) {
-            $inscription->setConfirmed(true);
-        }
-        $this->plainePresenceRepository->flush();
 
-        $facture = $this->facturePlaineHandler->newInstance($plaine, $tuteur);
-        $this->plainePresenceRepository->persist($facture);
-        $this->plainePresenceRepository->flush();
-
-        $this->facturePlaineHandler->handleManually($facture, $plaine);
-
-        $emails = TuteurUtils::getEmailsOfOneTuteur($tuteur);
-        if (\count($emails) < 1) {
-            $error = 'Pas de mail pour la facture plaine: '.$facture->getId();
-            $message = $this->adminEmailFactory->messageAlert('Erreur envoie facture', $error);
-            $this->notificationMailer->sendAsEmailNotification($message);
-            throw new Exception($error);
-        }
-
-        $from = $this->factureEmailFactory->getEmailAddressOrganisation();
-        $message = $this->factureEmailFactory->messageFacture($from, 'Inscription à la plaine', 'Coucou');
-        $this->factureEmailFactory->setTos($message, $emails);
-        $this->factureEmailFactory->attachFactureOnTheFly($facture, $message);
-
-        try {
-            $this->notificationMailer->sendMail($message);
-        } catch (TransportExceptionInterface $e) {
-            $error = 'Facture plaine num '.$facture->getId().' '.$e->getMessage();
-            $message = $this->adminEmailFactory->messageAlert('Erreur envoie facture plaine', $error);
-            $this->notificationMailer->sendAsEmailNotification($message);
-        }
-
-        $this->notificationMailer->sendAsEmailNotification($message);
-        $facture->setEnvoyeA(implode(',', $emails));
-        $facture->setEnvoyeLe(new DateTime());
-        $this->plainePresenceRepository->flush();
     }
 }
