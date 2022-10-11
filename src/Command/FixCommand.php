2270edb1f838b99f5158d11e1457bee2140d6c75
jfsenechal <jfsenechal@gmail.com>
2022-10-11 10:01:05 +0200

    for 31-08 and since 2019

--- src/Command/FixCommand.php
+++ src/Command/FixCommand.php
@@ -2,9 +2,12 @@
 
 namespace AcMarche\Mercredi\Command;
 
-use AcMarche\Mercredi\Facture\Handler\FacturePlaineHandler;
+use AcMarche\Mercredi\Facture\Handler\FactureHandler;
+use AcMarche\Mercredi\Facture\Repository\FacturePresenceRepository;
 use AcMarche\Mercredi\Facture\Repository\FactureRepository;
-use AcMarche\Mercredi\Plaine\Repository\PlaineRepository;
+use AcMarche\Mercredi\Jour\Repository\JourRepository;
+use AcMarche\Mercredi\Presence\Repository\PresenceRepository;
+use AcMarche\Mercredi\Presence\Utils\PresenceUtils;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -16,24 +19,69 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 )]
 class FixCommand extends Command
 {
+    private SymfonyStyle $io;
+
     public function __construct(
         private FactureRepository $factureRepository,
-        private FacturePlaineHandler $facturePlaineHandler,
-        private PlaineRepository $plaineRepository
+        private JourRepository $jourRepository,
+        private PresenceRepository $presenceRepository,
+        private FactureHandler $factureHandler,
+        private FacturePresenceRepository $facturePresenceRepository
     ) {
         parent::__construct();
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $io = new SymfonyStyle($input, $output);
-        $plaine = $this->plaineRepository->find(32);
-        // $facture = $this->factureRepository->find(8);
+        $this->io = new SymfonyStyle($input, $output);
 
-        foreach ($this->factureRepository->findAll() as $facture) {
-            $this->facturePlaineHandler->handleManually($facture, $plaine);
+        /**
+         * janvier 2019 => juin 2022
+         */
+        $rows = $this->presenceRepository->findWithOutPaiement();
+        $grouped = PresenceUtils::groupByTuteur($rows);
+        foreach ($grouped as $data) {
+            $tuteur = $data['tuteur'];
+            $presences = $data['presences'];
+            foreach ($presences as $key => $presence) {
+                if ($this->facturePresenceRepository->findByPresence($presence)) {
+                  /*  $this->io->error(
+                        $presence->getId().' '.$presence->getJour()->getDateJour()->format(
+                            'Y-m-d'
+                        ).' '.$presence->getEnfant()->getPrenom()
+                    );*/
+                    unset($presences[$key]);
+                }
+            }
+            if (count($presences) > 0) {
+                $facture = $this->factureHandler->newFacture($tuteur);
+                $facture->setMois('2022-06');
+                $this->factureHandler->handleManuallyNotResolved($facture, $presences, []);
+            }
         }
+        $this->factureRepository->flush();
 
         return Command::SUCCESS;
+    }
+
+
+    private function attach31Aout()
+    {
+        $jour = $this->jourRepository->find(448);//31 aout 2022
+        $presences = $this->presenceRepository->findByDay($jour);
+
+        foreach ($presences as $presence) {
+            $facture = $this->factureRepository->findFacturesByTuteurAndMonth($presence->getTuteur(), '09-2022');
+            if ($facture) {
+                $this->io->success($presence->getEnfant()->getNom().' '.$presence->getEnfant()->getPrenom());
+                if (!$this->facturePresenceRepository->findByPresence($presence)) {
+                    $this->factureHandler->attachPresences($facture, [$presence]);
+                    $this->factureRepository->flush();
+                }
+            } else {
+                //ne sont venu que le 31 aout et pas en septembre
+                $this->io->error($presence->getEnfant()->getNom().' '.$presence->getEnfant()->getPrenom());
+            }
+        }
     }
 }
