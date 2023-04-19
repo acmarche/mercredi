dc6a166b9eb1c9f63ea8e8661f0c693757517164
jfsenechal <jfsenechal@gmail.com>
2023-04-19 10:13:52 +0200

    add debug command

---
+++ src/Command/OrdreCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace AcMarche\Mercredi\Command;
+
+use AcMarche\Mercredi\Presence\Repository\PresenceRepository;
+use AcMarche\Mercredi\Relation\Utils\OrdreService;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'mercredi:ordre',
+)]
+class OrdreCommand extends Command
+{
+    private SymfonyStyle $io;
+
+    public function __construct(
+        private OrdreService $ordreService,
+        private PresenceRepository $presenceRepository,
+    ) {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('presence', InputArgument::REQUIRED, 'id presence');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->io = new SymfonyStyle($input, $output);
+
+        $presenceId = $input->getArgument('presence');
+        $presence = $this->presenceRepository->find($presenceId);
+
+        $this->io->writeln($presence->getEnfant());
+        $this->io->writeln($presence->getJour()->getDateJour()->format('Y-m-d'));
+        $ordre = $this->ordreService->getOrdreOnPresence($presence, true);
+        $presents = $this->ordreService->getFratriesPresents($presence);
+        foreach ($presents as $present) {
+            $this->io->writeln('Présent: '.$present);
+        }
+        $this->io->writeln('Ordre '.$ordre);
+
+        return Command::SUCCESS;
+    }
+
+
+}
