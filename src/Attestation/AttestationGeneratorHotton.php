16903b17aab2c7a06e78c770b4eb3818e90ffc35
jfsenechal <jfsenechal@gmail.com>
2023-05-09 15:43:10 +0200

    treatment

--- src/Attestation/AttestationGeneratorHotton.php
+++ src/Attestation/AttestationGeneratorHotton.php
@@ -7,7 +7,10 @@ use AcMarche\Mercredi\Contrat\Attestation\AttestationGeneratorInterface;
 use AcMarche\Mercredi\Contrat\Facture\FactureCalculatorInterface;
 use AcMarche\Mercredi\Contrat\Presence\PresenceCalculatorInterface;
 use AcMarche\Mercredi\Entity\Enfant;
+use AcMarche\Mercredi\Entity\Facture\Facture;
 use AcMarche\Mercredi\Entity\Tuteur;
+use AcMarche\Mercredi\Facture\Repository\FactureRepository;
+use AcMarche\Mercredi\Facture\Utils\FactureUtils;
 use AcMarche\Mercredi\Organisation\Traits\OrganisationPropertyInitTrait;
 use AcMarche\Mercredi\Presence\Repository\PresenceRepository;
 use Twig\Environment;
@@ -18,9 +21,11 @@ class AttestationGeneratorHotton implements AttestationGeneratorInterface
 
     public function __construct(
         private PresenceRepository $presenceRepository,
+        private FactureRepository $factureRepository,
         private AccueilRepository $accueilRepository,
         private PresenceCalculatorInterface $presenceCalculator,
         private FactureCalculatorInterface $factureCalculator,
+        private FactureUtils $factureUtils,
         private Environment $environment
     ) {
     }
@@ -28,14 +33,16 @@ class AttestationGeneratorHotton implements AttestationGeneratorInterface
     public function hasAttestation(Tuteur $tuteur, Enfant $enfant, int $year): bool
     {
         $presences = $this->presenceRepository->findByTuteurAndEnfantAndYear($tuteur, $enfant, $year);
+        $accueils = $this->accueilRepository->findByTuteurAndEnfantAndYear($tuteur, $enfant, $year);
 
-        return count($presences) === 0;
+        return count($presences) === 0 && count($accueils) === 0;
     }
 
     public function renderOne(Tuteur $tuteur, Enfant $enfant, int $year): string
     {
-        $presences = $this->presenceRepository->findByTuteurAndEnfantAndYear($tuteur, $enfant, $year);
-        $data = $this->treatment($presences);
+        $factures = $this->factureRepository->findByTuteurAndYear($tuteur, $year, true);
+
+        $data = $this->treatment($factures, $tuteur, $enfant);
 
         return $this->environment->render('@AcMarcheMercredi/admin/attestation/one/hotton/2022.html.twig', [
             'data' => $data,
@@ -49,38 +56,45 @@ class AttestationGeneratorHotton implements AttestationGeneratorInterface
 
     public function getDataByYear(int $year): array
     {
-        $presences = $this->presenceRepository->findByYear($year);
-
-        return $this->treatment($presences);
+        //todo
+        return [];
     }
 
-    private function treatment(array $presences): array
+    /**
+     * @param Facture[] $factures
+     * @param Tuteur $tuteur
+     * @param Enfant $enfant
+     * @return array
+     */
+    private function treatment(array $factures, Tuteur $tuteur, Enfant $enfant): array
     {
         $presencesPaid = [];
-        foreach ($presences as $presence) {
-            if ($this->factureCalculator->isPresencePaid($presence)) {
-                $presence->cout = $this->presenceCalculator->calculate($presence);
-                $presencesPaid[] = $presence;
+        foreach ($factures as $facture) {
+            foreach ($facture->getFacturePresences() as $facturePresence) {
+                if ($facturePresence->getObjectType() == Facture::OBJECT_ACCUEIL) {
+                    if ($accueil = $this->accueilRepository->find($facturePresence->getPresenceId())) {
+                        if ($accueil->getEnfant()->getId() == $enfant->getId()) {
+                            $accueil->cout = $facturePresence->getCoutCalculated();
+                            $presencesPaid[] = $accueil;
+                        }
+                    }
+                }
             }
-        }
 
-        $data = [];
-        foreach ($presencesPaid as $presence) {
-            $enfant = $presence->getEnfant();
-            $idEnfant = $enfant->getId();
-            $tuteur = $presence->getTuteur();
-            $idTuteur = $tuteur->getId();
-
-            $data[$idEnfant]['enfant'] = $enfant;
-            $data[$idEnfant]['tuteurs'][$idTuteur]['tuteur'] = $tuteur;
-            $data[$idEnfant]['tuteurs'][$idTuteur]['presences'][] = $presence;
-            if (!isset($data[$idEnfant]['tuteurs'][$idTuteur]['total'])) {
-                $data[$idEnfant]['tuteurs'][$idTuteur]['total'] = 0;
+            $data = [];
+            foreach ($presencesPaid as $presence) {
+                $idEnfant = $enfant->getId();
+                $idTuteur = $tuteur->getId();
+                $data[$idEnfant]['enfant'] = $enfant;
+                $data[$idEnfant]['tuteurs'][$idTuteur]['tuteur'] = $tuteur;
+                $data[$idEnfant]['tuteurs'][$idTuteur]['presences'][] = $presence;
+                if (!isset($data[$idEnfant]['tuteurs'][$idTuteur]['total'])) {
+                    $data[$idEnfant]['tuteurs'][$idTuteur]['total'] = 0;
+                }
+                $data[$idEnfant]['tuteurs'][$idTuteur]['total'] += $presence->cout;
             }
-            $data[$idEnfant]['tuteurs'][$idTuteur]['total'] += $presence->cout;
         }
 
         return $data;
     }
-
 }
