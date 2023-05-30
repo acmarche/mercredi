ba5a753883b2e28b3aeb291a82710d84df0fc9ff
jfsenechal <jfsenechal@gmail.com>
2023-05-30 11:33:30 +0200

    remove test mail

--- src/Controller/Admin/DefaultController.php
+++ src/Controller/Admin/DefaultController.php
@@ -7,8 +7,6 @@ use AcMarche\Mercredi\Search\Form\SearchAutocompleteType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
-use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
@@ -18,8 +16,8 @@ final class DefaultController extends AbstractController
 {
     use InitMailerTrait;
 
-    #[Route(path: '/{test}', name: 'mercredi_admin_home')]
-    public function default(Request $request, bool $test = false): Response
+    #[Route(path: '/', name: 'mercredi_admin_home')]
+    public function default(Request $request): Response
     {
         $form = $this->createForm(SearchAutocompleteType::class);
         $form->handleRequest($request);
@@ -34,31 +32,11 @@ final class DefaultController extends AbstractController
             }
         }
 
-        if ($test) {
-            $this->testmail();
-        }
-
         return $this->render(
             '@AcMarcheMercrediAdmin/default/index.html.twig',
             [
                 'form' => $form->createView(),
             ]
         );
-    }
-
-    private function testmail()
-    {
-        $message = new Email();
-        $message->subject('Test applicaiton mercredi');
-        $message->from("jf@atl-hotton.be");
-        $message->to("jfsenechal@gmail.com");
-        $message->text('Coucou, mail de test0');
-
-        try {
-            $this->sendMail($message);
-            var_dump(123);
-        } catch (TransportExceptionInterface $e) {
-            var_dump($e->getMessage());
-        }
     }
 }
