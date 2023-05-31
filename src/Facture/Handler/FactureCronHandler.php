17b77db182667fdef0c3237cc0dcc915e5e7b32f
jfsenechal <jfsenechal@gmail.com>
2023-05-31 10:38:32 +0200

    mode debug

--- src/Facture/Handler/FactureCronHandler.php
+++ src/Facture/Handler/FactureCronHandler.php
@@ -49,7 +49,7 @@ class FactureCronHandler
             $cron->setDateLastSync($now->modify('-5 minutes'));
             $this->factureCronRepository->flush();
 
-            $factures = $this->factureRepository->findFacturesByMonthNotSend($cron->getMonth());
+            $factures = $this->factureRepository->findFacturesByMonth($cron->getMonth());
             if (\count($factures) === 0) {
                 $cron->setDone(true);
                 $this->factureCronRepository->flush();
