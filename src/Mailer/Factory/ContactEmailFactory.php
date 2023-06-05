2409fc6620dc9d8a86028d967012fea45402021a
jfsenechal <jfsenechal@gmail.com>
2023-06-05 10:58:05 +0200

    clean

--- src/Mailer/Factory/ContactEmailFactory.php
+++ src/Mailer/Factory/ContactEmailFactory.php
@@ -5,16 +5,12 @@ namespace AcMarche\Mercredi\Mailer\Factory;
 use AcMarche\Mercredi\Mailer\InitMailerTrait;
 use AcMarche\Mercredi\Mailer\NotificationEmailJf;
 use AcMarche\Mercredi\Organisation\Traits\OrganisationPropertyInitTrait;
-use Symfony\Bridge\Twig\Mime\NotificationEmail;
 
 class ContactEmailFactory
 {
     use InitMailerTrait;
     use OrganisationPropertyInitTrait;
 
-    /**
-     * @return NotificationEmail
-     */
     public function sendContactForm(string $from, string $nom, string $body): NotificationEmailJf
     {
         $message = NotificationEmailJf::asPublicEmailJf();
