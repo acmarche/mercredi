9422e497376640f2e55aa7cfcb33266726119db9
jfsenechal <jfsenechal@gmail.com>
2023-10-25 15:42:08 +0200

    add test reduction

--- src/Tests/Calculator/CalculatorMarcheTest.php
+++ src/Tests/Calculator/CalculatorMarcheTest.php
@@ -8,6 +8,7 @@ use AcMarche\Mercredi\Entity\Enfant;
 use AcMarche\Mercredi\Entity\Jour;
 use AcMarche\Mercredi\Entity\Plaine\Plaine;
 use AcMarche\Mercredi\Entity\Presence\Presence;
+use AcMarche\Mercredi\Entity\Reduction;
 use AcMarche\Mercredi\Entity\Tuteur;
 use DateTime;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
@@ -70,6 +71,34 @@ class CalculatorMarcheTest extends KernelTestCase
         self::assertSame(1.0, $calculator->getPrixByOrdre($presence, 4));
     }
 
+    public function testReduction(): void
+    {
+        $container = static::getContainer();
+        $calculator = $container->get(PresenceCalculatorInterface::class);
+        $tuteur = new Tuteur();
+        $tuteur->id = 1;
+        $enfant = new Enfant();
+        $enfant->id = 1;
+        $jour = new Jour();
+        $jour->setPrix1(7);
+        $reduction = new Reduction();
+        $reduction->amount = 4;
+        $reduction->is_forfait = true;
+
+        $presence = new Presence($tuteur, $enfant, $jour);
+        $presence->setReduction($reduction);
+        self::assertSame(4.0, $calculator->calculate($presence));
+
+        $reduction->is_forfait = false;
+        $presence->setReduction($reduction);
+        self::assertSame(3.0, $calculator->calculate($presence));
+
+        $reduction->amount = null;
+        $reduction->pourcentage = 10;
+        $presence->setReduction($reduction);
+        self::assertSame(6.3, $calculator->calculate($presence));
+    }
+
     public function testPlaine()
     {
         $calculator = $this->getCalculator();
@@ -78,18 +107,24 @@ class CalculatorMarcheTest extends KernelTestCase
         $plaine->setPrix2(2);
         $plaine->setPrix3(1);
 
-        $jour = new  Jour(new DateTime());
+        $jour = new Jour(new DateTime());
         $jour->setPrix1(3);
         $jour->setPrix2(2);
         $jour->setPrix3(1);
         $jour->setPlaine($plaine);
 
-        $presence = new Presence(new Tuteur(), new Enfant(), $jour);
-        self::assertSame(3.0, $calculator->calculatePlaine($presence, 0));
-        self::assertSame(3.0, $calculator->calculatePlaine($presence, 1));
-        self::assertSame(2.0, $calculator->calculatePlaine($presence, 2));
-        self::assertSame(1.0, $calculator->calculatePlaine($presence, 3));
-        self::assertSame(1.0, $calculator->calculatePlaine($presence, 4));
+        $tuteur = new Tuteur();
+        $tuteur->id = 1;
+
+        $enfant = new Enfant();
+        $enfant->id = 1;
+
+        $presence = new Presence($tuteur, $enfant, $jour);
+        self::assertSame(3.0, $calculator->calculate($presence));
+        self::assertSame(3.0, $calculator->calculate($presence));
+        self::assertSame(2.0, $calculator->calculate($presence));
+        self::assertSame(1.0, $calculator->calculate($presence));
+        self::assertSame(1.0, $calculator->calculate($presence));
     }
 
     private function getCalculator(): PresenceCalculatorInterface
