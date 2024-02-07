905308a308422534f31ef66d59fc72ea3964bba5
jfsenechal <jfsenechal@gmail.com>
2024-02-07 11:42:02 +0100

    fix enfant id in facture presence

--- src/Command/FixCommand.php
+++ src/Command/FixCommand.php
@@ -2,11 +2,9 @@
 
 namespace AcMarche\Mercredi\Command;
 
-use AcMarche\Mercredi\Entity\Plaine\PlaineGroupe;
-use AcMarche\Mercredi\Entity\Scolaire\GroupeScolaire;
-use AcMarche\Mercredi\Plaine\Repository\PlaineGroupeRepository;
-use AcMarche\Mercredi\Plaine\Repository\PlaineRepository;
-use AcMarche\Mercredi\Scolaire\Repository\GroupeScolaireRepository;
+use AcMarche\Mercredi\Enfant\Repository\EnfantRepository;
+use AcMarche\Mercredi\Entity\Facture\FacturePresence;
+use AcMarche\Mercredi\Facture\Repository\FacturePresenceRepository;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -19,14 +17,10 @@ use Symfony\Component\Console\Style\SymfonyStyle;
 class FixCommand extends Command
 {
     private SymfonyStyle $io;
-    private GroupeScolaire $grands;
-    private GroupeScolaire $moyens;
-    private GroupeScolaire $petits;
 
     public function __construct(
-        private PlaineRepository $plaineRepository,
-        private PlaineGroupeRepository $plaineGroupeRepository,
-        private GroupeScolaireRepository $groupeScolaireRepository
+        private FacturePresenceRepository $facturePresenceRepository,
+        private EnfantRepository $enfantRepository
     ) {
         parent::__construct();
     }
@@ -34,53 +28,113 @@ class FixCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $this->io = new SymfonyStyle($input, $output);
-        $this->petits = $this->groupeScolaireRepository->find(2);
-        $this->moyens = $this->groupeScolaireRepository->find(3);
-        $this->grands = $this->groupeScolaireRepository->find(4);
+        //id: 2674 => enfantId= 1455
+        foreach ($this->facturePresenceRepository->findAll() as $facturePresence) {
+            if (!$facturePresence->enfantId) {
 
-        foreach ($this->plaineRepository->findAll() as $plaine) {
-            foreach ($this->plaineGroupeRepository->findByPlaine($plaine) as $groupePlaine) {
-                $groupe = $this->findGroupe($groupePlaine);
-                if (!$groupe) {
-                    $this->io->error($groupePlaine->getGroupeScolaire()->getNom());
-                    $this->io->error($groupePlaine->getGroupeScolaire()->getId());
+                if ($this->nohamCollas($facturePresence)) {
                     continue;
                 }
-                if (!$this->plaineGroupeRepository->findOneByPlaineAndGroupe($plaine, $groupe)) {
-                    $plaineGroupe = new PlaineGroupe($plaine, $groupe);
-                    $plaineGroupe->setInscriptionMaximum($groupePlaine->getInscriptionMaximum());
-                    $this->plaineGroupeRepository->persist($plaineGroupe);
+
+                if ($this->rojaMustafta($facturePresence)) {
+                    continue;
+                }
+                if ($this->aureliaMowack($facturePresence)) {
+                    continue;
+                }
+
+                $enfant = $this->enfantRepository->findOneBy(
+                    ['nom' => $facturePresence->getNom(), 'prenom' => $facturePresence->getPrenom()]
+                );
+
+                if ($enfant) {
+                    $facturePresence->enfantId = $enfant->getId();
+                } else {
+
+                    $this->error($facturePresence);
+
                 }
             }
         }
 
-        $this->plaineGroupeRepository->flush();
+        $this->facturePresenceRepository->flush();
 
         return Command::SUCCESS;
     }
 
-    private function findGroupe(PlaineGroupe $plaineGroupe): ?GroupeScolaire
+    private function aureliaMowack(FacturePresence $facturePresence): bool
     {
-        if (str_contains($plaineGroupe->getGroupeScolaire()->getNom(), 'petits')) {
-            return $this->petits;
-        }
-        if (str_contains($plaineGroupe->getGroupeScolaire()->getNom(), 'Moyens')) {
-            return $this->moyens;
-        }
-        if (str_contains($plaineGroupe->getGroupeScolaire()->getNom(), 'grands')) {
-            return $this->grands;
-        }
-        if ($plaineGroupe->getGroupeScolaire()->getId() == 6) {
-            return $this->moyens;
-        }
-        if ($plaineGroupe->getGroupeScolaire()->getId() == 4) {
-            return $this->grands;
-        }
-        if ($plaineGroupe->getGroupeScolaire()->getId() == 2) {
-            return $this->petits;
+        if (in_array($facturePresence->getFacture()->getId(), [1656])) {
+            if ($facturePresence->getPrenom() == 'Arthur') {
+                $enfant = $this->enfantRepository->findOneBy(
+                    ['nom' => 'GHEYS', 'prenom' => $facturePresence->getPrenom()]
+                );
+                if ($enfant) {
+                    $facturePresence->enfantId = $enfant->getId();
+                } else {
+                    $this->error($facturePresence);
+                }
+            }
+            if ($facturePresence->getPrenom() == 'Aurélia') {
+                $enfant = $this->enfantRepository->findOneBy(
+                    ['nom' => 'NOWAK', 'prenom' => $facturePresence->getPrenom()]
+                );
+                if ($enfant) {
+                    $facturePresence->enfantId = $enfant->getId();
+                } else {
+                    $this->error($facturePresence);
+                }
+
+            }
+
+            return true;
         }
 
-        return null;
+        return false;
     }
 
+    private function rojaMustafta(FacturePresence $facturePresence): bool
+    {
+        if (in_array($facturePresence->getFacture()->getId(), [304])) {
+            $enfant = $this->enfantRepository->findOneBy(
+                ['nom' => 'MUSTAFTAPUR', 'prenom' => $facturePresence->getPrenom()]
+            );
+            if ($enfant) {
+                $facturePresence->enfantId = $enfant->getId();
+            } else {
+                $this->error($facturePresence);
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private function nohamCollas(FacturePresence $facturePresence): bool
+    {
+        if (in_array($facturePresence->getFacture()->getId(), [128, 234, 372, 479, 591, 751])) {
+            $enfant = $this->enfantRepository->findOneBy(
+                ['nom' => 'GASMI', 'prenom' => $facturePresence->getPrenom()]
+            );
+            if ($enfant) {
+                $facturePresence->enfantId = $enfant->getId();
+            } else {
+                $this->error($facturePresence);
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private function error(FacturePresence $facturePresence)
+    {
+        $this->io->error(
+            'pas trouve '.$facturePresence->getFacture()->getId().' '.$facturePresence->getNom(
+            ).' '.$facturePresence->getPrenom()
+        );
+
+    }
 }
