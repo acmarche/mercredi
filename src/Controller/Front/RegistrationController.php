3bbf77eb80e2eb87b29c606f66e8a58ad5618b86
jfsenechal <jfsenechal@gmail.com>
2024-11-20 20:14:30 +0100

    fix google captcha

--- src/Controller/Front/RegistrationController.php
+++ src/Controller/Front/RegistrationController.php
@@ -16,7 +16,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 
 final class RegistrationController extends AbstractController
