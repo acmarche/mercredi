81744e7be5d84425c7119865ad867a73eea77bf8
jfsenechal <jfsenechal@gmail.com>
2025-02-07 13:40:45 +0100

    add reasons sante checker

--- src/Entity/Sante/Traits/SanteFicheTrait.php
+++ src/Entity/Sante/Traits/SanteFicheTrait.php
@@ -8,7 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
 trait SanteFicheTrait
 {
     #[ORM\OneToOne(targetEntity: SanteFiche::class, mappedBy: 'enfant', cascade: ['remove'])]
-    private ?SanteFiche $sante_fiche = null;
+    public ?SanteFiche $sante_fiche = null;
 
     public function getSanteFiche(): ?SanteFiche
     {
